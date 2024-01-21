1828423b8fb3cec5742a9ce791938b40160a8c8b
yanwu xu <yanwuxu@pitt.edu>
2024-01-21 17:37:45 -0500

    Update run_MOG.sh

--- run_MOG.sh
+++ run_MOG.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source /ocean/projects/asc170022p/yanwuxu/miniconda/etc/profile.d/conda.sh
 conda activate ldm
 
 python Two_Dimensional_MOG_ddgan_continuous_siddms.py --num_timesteps 4  --ac_w 1.0
